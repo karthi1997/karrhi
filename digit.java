79e3f416369d2d4cc7b4f187e47d8fdf8bca6c7a
karthi1997 <karthiselvam.777@gmail.com>
2017-06-10 16:54:34 +0530

    Create digit.java

---
+++ digit.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class digit
+{
+public static void main(String[] arg)
+{
+   Scanner s=new Scanner(System.in);
+   int n=s.nextInt();
+   if(n>0)
+    System.out.println("positive");
+    else
+    if(n<0)
+     System.out.println("negative);
+     else
+     System.out.println("zero");
+     }
+     }
