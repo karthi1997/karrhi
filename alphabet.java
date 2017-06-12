e4c3ce7ca1e2afa3e2ccea1cedb61840faeeb010
karthi1997 <karthiselvam.777@gmail.com>
2017-06-12 20:15:38 +1100

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class alphabet
+{
+ public static void main(String[] arg)
+ {
+  Scanner s=new Scanner(System.in);
+  String s1=s.nextLine();
+  char ch=s1.charAt(0);
+  if(ch>=65 && ch<=95 || ch>=97 && ch<=122)
+  {
+   System.out.print("alpha");
+   }
+   else
+ {
+    System.out.print("not alpha");
+}
+   }
+   }
+   
