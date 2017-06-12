1a2e800da6a4320995fb69ac0974dc817fc506c6
karthi1997 <karthiselvam.777@gmail.com>
2017-06-12 19:25:35 +1100

    Create vowel.java

---
+++ vowel.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class vowel
+{
+public static void main(String[] arg)
+{
+ Scanner s=new Scanner(System.in);
+ String s1=s.nextLine();
+ int flag=0,count=0;
+ char[] c=s1.toCharArray();
+ for(int i=0;i<c.length;i++)
+ {
+     if(flag==0)
+ {
+ if(c[i]=='a'||c[i]=='e'||c[i]=='i'||c[i]=='o'||c[i]=='u')
+ {
+  flag++;
+  count++;
+ }
+       }
+ }
+ if(count==1)
+ System.out.print("vowels");
+ else
+    System.out.println("constants");
+    
+}
+    }
