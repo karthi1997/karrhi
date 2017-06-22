3df2e6c076b419039528d971d96d8d83341c4bb7
karthi1997 <karthiselvam.777@gmail.com>
2017-06-21 21:46:46 -0700

    Create conversion.java

---
+++ conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class conversion
+{
+public static void main(String[] args)
+{
+ Scanner sc=new Scanner(System.in);
+ String s=sc.nextLine();
+ int n=Integer.parseInt(s);
+ System.out.println(n);
+ }
+ }
+ 
